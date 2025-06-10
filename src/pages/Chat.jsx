02a0c2bd6ce3023eabe22b0e0b43a4d6c5d8cf8d
Akash Patel <117297200+luminarakash@users.noreply.github.com>
2025-06-10 11:29:25 +0530

    Chat.jsx

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -189,7 +189,7 @@ const Chat = ({ chatId, user }) => {
       >
         {/* Background image fixed behind everything */}
         <img
-          src="/src/assets/i.png"
+          src="/src/assets/Hii.png"
           alt="EImage"
           className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-0 w-screen h-screen p-10 object-contain pointer-events-none opacity-30"
         />
